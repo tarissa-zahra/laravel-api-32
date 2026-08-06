c65f47b967c2c3b12b826001d7a353b329876cdd
ica <tarissa.zahra3@smk.belajar.id>
2026-08-06 08:05:38 +0700

    Method delete

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
 Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
+Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
